f4191688a1b433ab7860a8c072a3f7030373b231
Minho Kwon <minhok3@gmail.com>
2022-04-24 19:37:04 -0400

    fetch fix

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
   const weatherData = useSelector((state) => state);
 
   const makeFetchCall = useCallback(() => {
-    fetch("https://localhost:4000/", {
+    fetch("http://localhost:4000/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
